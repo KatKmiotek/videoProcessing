5daa1aa5bea41fa24e31bdd0d0ac4ed45e352382
Katarzyna Kmiotek <katarzyna.kmiotek@2itesting.com>
2021-03-24 17:12:10 +0000

    login instructions added

--- components/protectedPage.js
+++ components/protectedPage.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {Container, Button, Input, Stack} from '@chakra-ui/react'
+import {Container, Button, Input, Stack, Box, Heading, Link, Image, ExternalLinkIcon, Text} from '@chakra-ui/react'
 import Header from './header'
 import {useAuth} from '../hooks/'
 
@@ -50,6 +50,24 @@ export default function ProtectedPage({children}) {
             />
           </Stack>
           <Button onClick={() => loginToSymbl()} bg="#5BD9AB">Login</Button>
+          <Box boxShadow="dark-lg" p="6" marginTop="20" rounded="md" bg="white">
+            <Container margin="1rem">
+            <Text>
+              <Heading as="h4" size="sm" marginBottom="5">
+                Login Instructions:
+              </Heading>
+              1) Please create a free account on <Link href="https://symbl.ai/" isExternal>
+               <strong>Symbl.ai</strong></Link> website. <br/>
+
+              2) To login use API credentials provided in the HOME tab<br/>
+               <Image src="homeTab.png" marginTop="2" marginBottom="2"/>
+              3) App made as part of <Link href="https://egghead.io/courses/create-contextual-video-analysis-app-with-nextjs-and-symbl-ai-4efb" is isExternal>
+              <strong> EggHead.io </strong></Link> course 
+               </Text>
+              
+              
+            </Container>
+          </Box>
         </Container>
       ) : (
         children
